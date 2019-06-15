945cf4a2beef9372d53af3b2869e92fc713b1d41
JangMyungJoo <wkdaudwn11@naver.com>
2019-06-15 12:20:48 +0900

    Add home

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import Home from '../components/Home';
+import Posts from '../components/Posts';
 
 export default class App extends Component {
     render() {
         return (
             <div>
-                this is the app
+                <Route exact path="/" component={Home} />
+                <Route path="/posts" component={Posts} />
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Root from './components/Root';
 import App from './components/App';
-import Posts from './components/Posts';
-
 
 ReactDOM.render(
     <Root>
         <BrowserRouter>
             <Route path="/" component={App} />
-            <Route path="/posts" component={Posts} />
         </BrowserRouter>
     </Root>
     ,
