ca16be471ac76e95f0f27ecc0d14f59ef1294704
JangMyungJoo <wkdaudwn11@naver.com>
2019-06-22 10:27:24 +0900

    Updated Nav

--- src/components/common/HeaderNav.jsx
+++ src/components/common/HeaderNav.jsx
@@ -4,10 +4,13 @@ import { Alignment, Navbar, Button } from '@blueprintjs/core';
 export default () => (
     <Navbar>
         <Navbar.Group align={Alignment.LEFT}>
-            <Navbar.Heading>Blueprint</Navbar.Heading>
+            <Navbar.Heading>Firebase App</Navbar.Heading>
             <Navbar.Divider />
             <Button className="bp3-minimal" icon="home" text="Home" />
-            <Button className="bp3-minimal" icon="document" text="Files" />
+        </Navbar.Group>
+        <Navbar.Group align={Alignment.RIGHT}>
+            <Button className="bp3-minimal" text="Sign Up" />
+            <Button className="bp3-minimal" text="Sign In" />
         </Navbar.Group>
     </Navbar>
 );
